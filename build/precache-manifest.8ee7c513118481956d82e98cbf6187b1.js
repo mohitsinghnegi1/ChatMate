e9d25471f1752e2b964d879cfbc5130c164e37a6
mohitsinghnegi1 <mohitsin3232@gmail.com>
2020-06-17 22:04:50 +0530

    added build file

---
+++ build/precache-manifest.8ee7c513118481956d82e98cbf6187b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e3bef25f8a047619fcdb1aea73f53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5f821673f05ae9546a2",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0ecafe96ff12ef985f18",
+    "url": "/static/css/main.5442940e.chunk.css"
+  },
+  {
+    "revision": "d5f821673f05ae9546a2",
+    "url": "/static/js/2.ca055ff2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ca055ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecafe96ff12ef985f18",
+    "url": "/static/js/main.edb67789.chunk.js"
+  },
+  {
+    "revision": "edc17b39de4a11c7f4c0",
+    "url": "/static/js/runtime-main.b21ae222.js"
+  },
+  {
+    "revision": "be2cf3cb93aeb1cd66a2ffcffabd56a7",
+    "url": "/static/media/logo.be2cf3cb.png"
+  },
+  {
+    "revision": "bc704c78304b6d8415eb0b2c874928f3",
+    "url": "/static/media/pattern.bc704c78.jpg"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/static/media/sender_avatar.e67e358c.jpg"
+  },
+  {
+    "revision": "cd3b95343304ca7bbd8771389abc4386",
+    "url": "/static/media/start_chat.cd3b9534.svg"
+  },
+  {
+    "revision": "d75f0f17c2f2a5bcc7452dcaa859fc35",
+    "url": "/static/media/user.d75f0f17.png"
+  },
+  {
+    "revision": "56a8018d4b90e248dc6f239794450813",
+    "url": "/static/media/workchat.56a8018d.svg"
+  }
+]);
